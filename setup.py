ebaabbd6a7903b0949c05a5bcb7658fb20b3963a
JeongYeon Nam <tonynamy@apperz.co.kr>
2023-08-17 18:30:02 +0900

    Update project urls

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", encoding="utf-8") as f:
 setup(
     name="typed_graphene",
     packages=find_packages(include=["typed_graphene", "typed_graphene.*"]),
-    version="0.0.3",
+    version="0.0.4",
     description="Type-safe interface for graphene-python.",
     long_description=long_description,
     long_description_content_type="text/markdown",
@@ -16,4 +16,9 @@ setup(
     setup_requires=["graphene>=2.0.0, <3"],
     tests_require=["pytest==7.4.0"],
     test_suite="tests",
+    project_urls={
+        "Source": "https://github.com/tonynamy/typed-graphene",
+        "Documentation": "https://github.com/tonynamy/typed-graphene",
+        "Tracker": "https://github.com/tonynamy/typed-graphene/issues",
+    },
 )
